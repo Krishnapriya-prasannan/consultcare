8ea354e15bb3ce8af8046ff9a2d527f6660236f2
Krishnapriya Prasannan <krishnapriyaprasannan1@gmail.com>
2024-10-06 11:04:19 +0530

    server.js modified

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,31 @@
+require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2'); // Import the mysql2 package
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
 
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Define the dbConfig object
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
 
-const db = mysql.createConnection({
-  socketPath: '/var/run/mysqld/mysqld.sock',
-  host: 'localhost',
-  user: 'web', 
-  password: 'mysql12', 
-  database: 'ConsultcareDB' 
-});
+// Check if the socket path is defined and add it to the configuration
+if (process.env.DB_SOCKET_PATH) {
+  dbConfig.socketPath = process.env.DB_SOCKET_PATH;
+}
+
+// Create the database connection
+const db = mysql.createConnection(dbConfig);
 
 db.connect((err) => {
   if (err) {
@@ -28,7 +35,7 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-  });
-  
+  console.log(`Server running on http://localhost:${port}`);
+});
